a6c0fccad73666499f7ff3f7e1ee2777f09f4735
eat-sleep-code <info@eat-sleep-code.com>
2023-10-27 08:52:12 -0700

    Update Framework

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Binary Data Extractor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The Binary Data Extractor is a Windows desktop utility that can extract files that have been stored in a Microsoft SQL database as Binary Large Object (BLOB).")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("<eat-sleep-code />")]
 [assembly: AssemblyProduct("Binary Data Extractor")]
-[assembly: AssemblyCopyright("Copyright © <eat-sleep-code />  2015")]
+[assembly: AssemblyCopyright("Copyright © <eat-sleep-code />  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2023.10.27.8")]
+[assembly: AssemblyFileVersion("2023.10.27.8")]
